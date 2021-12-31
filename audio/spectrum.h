f6edc9373b6f79f1668469e01b5eb165ce079788
Konstantin Kutsenko <ssidein@gmail.com>
2021-12-31 21:16:20 +0300

    Spectrum ref p1

--- audio/spectrum.h
+++ audio/spectrum.h
@@ -43,66 +43,34 @@
 
 #include <qglobal.h>
 #include "utils.h"
-//#include "fft/fftreal_wrapper.h" // For FFTLengthPowerOfTwo
-
-//-----------------------------------------------------------------------------
-// Constants
-//-----------------------------------------------------------------------------
 
 const int FFTLengthPowerOfTwo = 12;
-
-// Number of audio samples used to calculate the frequency spectrum
 const int    SpectrumLengthSamples  = PowerOfTwo<FFTLengthPowerOfTwo>::Result;
-
-// Number of bands in the frequency spectrum
 const int    SpectrumNumBands       = 50;
-
-// Lower bound of first band in the spectrum
 const qreal  SpectrumLowFreq        = 20.0; // Hz
-
-// Upper band of last band in the spectrum
 const qreal  SpectrumHighFreq       = 2000.0; // Hz
-
-// Waveform window size in microseconds
-const qint64 WaveformWindowDuration = 500 * 1000;
-
-// Length of waveform tiles in bytes
-// Ideally, these would match the QAudio*::bufferSize(), but that isn't
-// available until some time after QAudio*::start() has been called, and we
-// need this value in order to initialize the waveform display.
-// We therefore just choose a sensible value.
+const qint64 WaveformWindowDuration = 500 * 1000; //TODO rid of useless
 const int   WaveformTileLength      = 4096;
-
-// Fudge factor used to calculate the spectrum bar heights
 const qreal SpectrumAnalyserMultiplier = 0.15;
-
-// Disable message timeout
 const int   NullMessageTimeout      = -1;
 
-
-//-----------------------------------------------------------------------------
-// Types and data structures
-//-----------------------------------------------------------------------------
 
 enum WindowFunction {
     NoWindow,
     HannWindow
 };
 
+
 const WindowFunction DefaultWindowFunction = HannWindow;
 
-struct Tone
-{
-    Tone(qreal freq = 0.0, qreal amp = 0.0)
-    :   frequency(freq), amplitude(amp)
-    { }
 
-    // Start and end frequencies for swept tone generation
-    qreal   frequency;
-
-    // Amplitude in range [0.0, 1.0]
-    qreal   amplitude;
+struct Tone {
+    Tone(qreal freq = 0.0, qreal amp = 0.0):frequency(freq), amplitude(amp)
+    {}
+    qreal   frequency; // Start and end frequencies for swept tone generation
+    qreal   amplitude; // Amplitude in range [0.0, 1.0]
 };
+
 
 struct SweptTone
 {
@@ -123,22 +91,10 @@ struct SweptTone
 };
 
 
-//-----------------------------------------------------------------------------
-// Macros
-//-----------------------------------------------------------------------------
-
-// Macro which connects a signal to a slot, and which causes application to
-// abort if the connection fails.  This is intended to catch programming errors
-// such as mis-typing a signal or slot name.  It is necessary to write our own
-// macro to do this - the following idiom
-//     Q_ASSERT(connect(source, signal, receiver, slot));
-// will not work because Q_ASSERT compiles to a no-op in release builds.
-
 #define CHECKED_CONNECT(source, signal, receiver, slot) \
     if (!connect(source, signal, receiver, slot)) \
         qt_assert_x(Q_FUNC_INFO, "CHECKED_CONNECT failed", __FILE__, __LINE__);
 
-// Handle some dependencies between macros defined in the .pro file
 
 #ifdef DISABLE_WAVEFORM
 #undef SUPERIMPOSE_PROGRESS_ON_WAVEFORM
