c3c163b0df17ea00ad1aa12ac2da41facfc9cf98
Konstantin Kutsenko <ssidein@gmail.com>
2021-12-31 20:07:18 +0300

    Feature extractor tiny refactoring

--- audio/featureextractor.cpp
+++ audio/featureextractor.cpp
@@ -2,16 +2,16 @@
 
 #include <cmath>
 
+#include <QDebug>
+
 #include "yin.h"
 
-#include <QDebug>
 
 
 double calc_RMS(const float* data, const size_t len) {
     double squareAmpSum = 0.0;
-    for (size_t i = 0; i < len; ++i) {
+    for (size_t i = 0; i < len; ++i)
         squareAmpSum += data[i] * data[i];
-    }
     return  std::sqrt(squareAmpSum / static_cast<double>(len));
 }
 
@@ -26,11 +26,10 @@ double calc_dB(const float* data, const size_t len) {
 
 
 double calc_MidiCents(double f0) {
-    double note = 69 + log(f0 / 440.f) * 17.31234; //Some evil magic from lib_extract
-    return floor( 0.5f + note * 100 ); //TODO check round(note * 100)?
+    double note = 69 + log(f0 / 440.f) * 17.31234; //TODO rewrite evil magic
+    return floor( 0.5f + note * 100 );
 }
 
-//TODO single function with static object
 
 double calc_YinF0(const float* data, const size_t len, double sampleRate) {
     static Yin yin;
--- audio/featureextractor.h
+++ audio/featureextractor.h
@@ -3,14 +3,12 @@
 
 #include <string> //Window names in future, now for size_t :)
 
-
 double calc_RMS(const float* data, const size_t len);
 double calc_dB(const float* data, const size_t len);
 
 double calc_MidiCents(double f0);
-
 double calc_YinF0(const float* data, const size_t len, double sampleRate=44100.0);
 
-//TODO libextract features slop, sharp etc to find ADSR?
+//TODO mean, median, envelop
 
 #endif // FEATUREEXTRACTOR_H
