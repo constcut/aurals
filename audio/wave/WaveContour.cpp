558378cd9af653bb4862b9ab3f01f6a886d8427b
Konstantin Kutsenko <ssidein@gmail.com>
2022-01-17 17:59:43 +0300

    Some dirty color map..

--- audio/wave/WaveContour.cpp
+++ audio/wave/WaveContour.cpp
@@ -11,6 +11,7 @@
 #include "libs/kiss/kiss_fftr.h"
 #include "libs/fft/FFTurealfix.hpp"
 #include <QImage>
+#include <QVector3D>
 
 
 using namespace aural_sight;
@@ -109,8 +110,21 @@ inline float hannWindowFun(float t, size_t N) {
 
 
 
+const double scalar32[32] = {0,0.032258065,0.064516129,0.096774194,0.129032258,0.161290323,0.193548387,0.225806452,0.258064516,0.290322581,0.322580645,0.35483871,0.387096774,0.419354839,0.451612903,0.483870968,0.516129032,0.548387097,0.580645161,0.612903226,0.64516129,0.677419355,0.709677419,0.741935484,0.774193548,0.806451613,0.838709677,0.870967742,0.903225806,0.935483871,0.967741935,1};
+
+const int redDawn32[32] = {0,0,1,1,1,1,2,2,2,3,3,3,4,4,4,4,5,5,63,125,186,248,255,255,255,255,255,255,255,255,255,255};
+const int greenDawn32[32] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,15,98,181,255,255,255,255};
+const int blueDawn32[32] = {0,11,23,35,46,58,69,81,93,104,116,128,139,151,162,174,185,197,211,225,239,254,193,124,56,1,7,12,23,100,178,255};
+
+
 //TODO sepparate it somewhere
 void WaveContour::STFT(QString filename) {
+
+
+    std::vector<QColor> colorVec;
+
+    for (size_t i = 0; i < 32; ++i)
+        colorVec.push_back(QColor(redDawn32[i], greenDawn32[i], blueDawn32[i]));
 
     const size_t windowSize = 4096*2;
     const size_t windowStep = 64; //TODO 512 or 256 even
@@ -147,6 +161,9 @@ void WaveContour::STFT(QString filename) {
     size_t height = windowSize / specCut;
     float hScale = log(height) / height;
 
+    float minA = std::numeric_limits<float>::max();
+    float maxA = std::numeric_limits<float>::min();
+
     size_t count = 0;
     size_t position = 0;
     while (position + windowSize < _floatSamples.size()) {
@@ -169,24 +186,33 @@ void WaveContour::STFT(QString filename) {
             const float amplitude = 0.15 * std::log(magnitude);
             amp[i] = amplitude;
 
-            //const float simpleG = 255 * (amplitude); //log10(magnitude);
-            //QColor color(0, simpleG, simpleG / 4.0); //.rgb
-            //img.setPixel(count, windowSize/specCut - i, color.rgb());
+            if (amplitude > maxA)
+                maxA = amplitude;
+
+            if (amplitude < minA)
+                minA = amplitude;
+
+            /*
+            float c = 255 * (a);
+            if (a < 0.2)
+                c /= 4;
+            QColor color(0, c , c / 4);*/
         }
 
         for (size_t i = 0; i < outKiss.size() / specCut; ++i) {
+
             int index = pow(2.7182818284590452354, i * hScale);
-            const float a = amp[index];
-            //const float simpleG = 255 * (a);
+            float a = amp[index];
 
-            //const float logA = log10(a);
-            float c = 255 * (a);
+            a = (a - minA) / (maxA - minA);
+            a *= 0.95;
 
-            if (a < 0.2)
-                c /= 4;
+            size_t j;
+            for (j = 0; j < 32; ++j)
+                if (scalar32[j] > a)
+                    break;
 
-            QColor color(0, c , c / 4);
-            img.setPixel(count, windowSize/specCut - i, color.rgb());
+            img.setPixel(count, windowSize/specCut - i, colorVec[j].rgb());
         }
 
 
@@ -198,6 +224,8 @@ void WaveContour::STFT(QString filename) {
     auto durationMs = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
     qDebug() << "Spent " << durationMs / 1000.0
              << " on whole STFT "  << count << " steps";
+
+    qDebug() << minA << " " << maxA;
 
     img.save(filename);
 }
