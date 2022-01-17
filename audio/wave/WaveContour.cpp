6b845ab6dc3d6a76e30e765661d06dc1a7231c68
Konstantin Kutsenko <ssidein@gmail.com>
2022-01-17 18:33:29 +0300

    Dirty but better mapping

--- audio/wave/WaveContour.cpp
+++ audio/wave/WaveContour.cpp
@@ -9,9 +9,7 @@
 
 
 #include "libs/kiss/kiss_fftr.h"
-#include "libs/fft/FFTurealfix.hpp"
 #include <QImage>
-#include <QVector3D>
 
 
 using namespace aural_sight;
@@ -109,22 +107,8 @@ inline float hannWindowFun(float t, size_t N) {
 }
 
 
-
-const double scalar32[32] = {0,0.032258065,0.064516129,0.096774194,0.129032258,0.161290323,0.193548387,0.225806452,0.258064516,0.290322581,0.322580645,0.35483871,0.387096774,0.419354839,0.451612903,0.483870968,0.516129032,0.548387097,0.580645161,0.612903226,0.64516129,0.677419355,0.709677419,0.741935484,0.774193548,0.806451613,0.838709677,0.870967742,0.903225806,0.935483871,0.967741935,1};
-
-const int redDawn32[32] = {0,0,1,1,1,1,2,2,2,3,3,3,4,4,4,4,5,5,63,125,186,248,255,255,255,255,255,255,255,255,255,255};
-const int greenDawn32[32] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,15,98,181,255,255,255,255};
-const int blueDawn32[32] = {0,11,23,35,46,58,69,81,93,104,116,128,139,151,162,174,185,197,211,225,239,254,193,124,56,1,7,12,23,100,178,255};
-
-
-//TODO sepparate it somewhere
 void WaveContour::STFT(QString filename) {
 
-
-    std::vector<QColor> colorVec;
-
-    for (size_t i = 0; i < 32; ++i)
-        colorVec.push_back(QColor(redDawn32[i], greenDawn32[i], blueDawn32[i]));
 
     const size_t windowSize = 4096*2;
     const size_t windowStep = 64; //TODO 512 or 256 even
@@ -132,17 +116,11 @@ void WaveContour::STFT(QString filename) {
 
     kiss_fftr_cfg cfg = kiss_fftr_alloc( windowSize, 0, 0, 0 );
 
-
-    FFTRealFixLen<12> fft_fix;
-
     size_t width = (_floatSamples.size() - windowSize) / windowStep;
-
-
-    size_t specCut = 4;
+    size_t specCut = 2;
 
     qDebug() << "Width " << width;
     QImage img(width + 1, (windowSize / specCut) + 1, QImage::Format_RGB32);
-
 
     auto start = std::chrono::high_resolution_clock::now();
 
@@ -151,7 +129,6 @@ void WaveContour::STFT(QString filename) {
         windowFun[i] = hannWindowFun(i, windowSize); //window[i]
 
     std::vector<float> windowed(windowSize);
-
 
     std::vector<kiss_fft_cpx> outKiss(windowSize); //TODO std::complex
     std::vector<float> outFix(windowSize);
@@ -174,16 +151,17 @@ void WaveContour::STFT(QString filename) {
             windowed[i] = winPos[i] * windowFun[i];
 
         kiss_fftr( cfg , windowed.data() , outKiss.data() );
-        //TODO image
-        //fft_fix.do_fft(outFix.data() , windowed.data());
-
 
 
         for (size_t i = 0; i < outKiss.size() / specCut; ++i) {
 
             const auto b = outKiss[i];
             const float magnitude = sqrt(b.i * b.i + b.r * b.r);
-            const float amplitude = 0.15 * std::log(magnitude);
+            float amplitude = 0.15 * std::log(magnitude);
+
+            amplitude = std::max(0.f, amplitude);
+            amplitude = std::min(1.f, amplitude);
+
             amp[i] = amplitude;
 
             if (amplitude > maxA)
@@ -192,11 +170,6 @@ void WaveContour::STFT(QString filename) {
             if (amplitude < minA)
                 minA = amplitude;
 
-            /*
-            float c = 255 * (a);
-            if (a < 0.2)
-                c /= 4;
-            QColor color(0, c , c / 4);*/
         }
 
         for (size_t i = 0; i < outKiss.size() / specCut; ++i) {
@@ -204,15 +177,28 @@ void WaveContour::STFT(QString filename) {
             int index = pow(2.7182818284590452354, i * hScale);
             float a = amp[index];
 
-            a = (a - minA) / (maxA - minA);
-            a *= 0.95;
+            int gColor = 256 * a;
+            int rColor = 0;
+            int bColor = 0;
 
-            size_t j;
-            for (j = 0; j < 32; ++j)
-                if (scalar32[j] > a)
-                    break;
+            if (gColor < 50)
+                gColor /= 10;
 
-            img.setPixel(count, windowSize/specCut - i, colorVec[j].rgb());
+            if (gColor > 80)
+                gColor += 64;
+
+            if (gColor > 255)
+                gColor = 255;
+
+            if (gColor > 150) //Only for current files :(
+                rColor = gColor / 3;
+
+            if (gColor > 100)
+                bColor = gColor / 2;
+
+            QColor color(rColor,gColor,bColor);
+
+            img.setPixel(count, windowSize/specCut - i, color.rgb());
         }
 
 
