ddd316361628ecfe802a933984b4722dcfd2bdcf
Konstantin Kutsenko <ssidein@gmail.com>
2022-02-08 12:45:32 +0300

    tweking hot fix, its here for a while

--- audio/wave/AudioHandler.cpp
+++ audio/wave/AudioHandler.cpp
@@ -245,7 +245,7 @@ void AudioHandler::startMidiPlayer() {
     const double bytesPerSample = _midiFormat.sampleSize() / 8.0;
     const double msInSecond = 1000.0;
     const double channels = 2.0;
-    const double ms = static_cast<double>(_midiBufer.size()) / (channels * bytesPerSample * sampleRate / msInSecond) - 100;
+    const double ms = static_cast<double>(_midiBufer.size()) / (channels * bytesPerSample * sampleRate / msInSecond) - 150;
     _midiPlayer->start();
     _midiOutput->start(_midiPlayer.get());
     _midiStopRequestTimer.start(ms);
