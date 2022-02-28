c3762ff78fae78f76941afbd39067234ef8d42dc
Konstantin Kutsenko <ssidein@gmail.com>
2022-03-01 00:27:40 +0300

    Bricks different sizes

--- music/PatternLine.cpp
+++ music/PatternLine.cpp
@@ -10,8 +10,7 @@ void PatternLine::paint(QPainter* painter) {
     if (_bricks.empty())
         updateBricks();
 
-    for (size_t i = 0; i < _bricks.size(); ++i)
-    {
+    for (size_t i = 0; i < _bricks.size(); ++i) {
         const auto& b = _bricks[i];
         if (b.on)
             painter->fillRect(b.x, b.y, b.w, b.h, QBrush(QColor("darkgreen")));
@@ -20,20 +19,28 @@ void PatternLine::paint(QPainter* painter) {
 }
 
 
-void PatternLine::updateBricks() {
+void PatternLine::updateBricks()
+{
    //Обновлять умней, чтобы та часть что не меняется - сохраняла паттерн
     double total = (static_cast<double>(_num) / _denom) / (1.0 / _brickSize);
     _bricks.clear();
-    for (size_t i = 0; i < total; ++i)
-        _bricks.push_back({static_cast<int>(i) * 30, 0, 25, 25});
+
+    const int brickHeight = 25;
+    const int brickWidth = 64 * 4 * (1.0 / _brickSize);
+    const int brickPadding = 8  * 4 * (1.0 / _brickSize);
+
+    for (size_t i = 0; i < total; ++i) //TODO correct distance in different sizes
+        _bricks.push_back({static_cast<int>(i) * (brickWidth + brickPadding),
+                           0, brickWidth, brickHeight});
 }
 
 
-void PatternLine::onClick(int x, int y) {
-    for (auto& b: _bricks) {
+void PatternLine::onClick(int x, int y)
+{
+    for (auto& b: _bricks)
         if (b.hit(x, y)) {
             b.on = !b.on;
+            break;
         }
-    }
     update();
 }
