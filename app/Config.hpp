3551c67d9700cf209c528cd62bff64402ef0cf8a
Konstantin Kutsenko <ssidein@gmail.com>
2022-03-04 18:05:12 +0300

    TableModel from qt examples

--- app/Config.hpp
+++ app/Config.hpp
@@ -9,6 +9,7 @@
 
 #include <QObject>
 #include <QAbstractTableModel>
+#include <qqml.h>
 
 
 namespace aurals {
@@ -107,6 +108,43 @@ namespace aurals {
             return Config::getInst().getValue(name.toStdString());
         }
 
+    };
+
+
+
+    class ConfigTableModel : public QAbstractTableModel
+    {
+        Q_OBJECT
+        QML_ELEMENT
+        QML_ADDED_IN_MINOR_VERSION(1)
+
+    public:
+        int rowCount(const QModelIndex & = QModelIndex()) const override
+        {
+            return 10;
+        }
+
+        int columnCount(const QModelIndex & = QModelIndex()) const override
+        {
+            return 5;
+        }
+
+        QVariant data(const QModelIndex &index, int role) const override
+        {
+            switch (role) {
+                case Qt::DisplayRole:
+                    return QString("%1, %2").arg(index.column()).arg(index.row());
+                default:
+                    break;
+            }
+
+            return QVariant();
+        }
+
+        QHash<int, QByteArray> roleNames() const override
+        {
+            return { {Qt::DisplayRole, "display"} };
+        }
     };
 
 }
