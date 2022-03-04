922289fcd692c012c50fcbc24a39125c65f22da8
Konstantin Kutsenko <ssidein@gmail.com>
2022-03-04 18:02:54 +0300

    Removed old guitarmy skins from config

--- app/Config.hpp
+++ app/Config.hpp
@@ -8,13 +8,14 @@
 #define CONF_PARAM(z) Config::getInst().values[ z ] //TODO maybe replace with QSettings
 
 #include <QObject>
+#include <QAbstractTableModel>
+
 
 namespace aurals {
 
 
     void setTestLocation(std::string newTL);
     void initGlobals();
-
 
 
 
@@ -107,98 +108,6 @@ namespace aurals {
         }
 
     };
-
-
-
-    class Skin //TODO colors palette + save congif
-    {
-    protected:
-        //just cfg lines
-        std::vector<std::string> configLines;
-    public:
-        Skin() { }
-        //
-        virtual void init()=0;
-
-        void setIntoConfig(Config& conf)
-        {
-            for (size_t i=0; i < configLines.size(); ++i)
-                conf.addLine(configLines[i]);
-        }
-    };
-
-    class DarkSkin : public Skin
-    {
-    public:
-        DarkSkin(){init();}
-        virtual void init()
-        {
-            configLines.push_back("colors.background = black");
-            configLines.push_back("colors.curBar = white");
-            configLines.push_back("colors.curBeat = gray");
-            configLines.push_back("colors.curString = darkgray");
-            configLines.push_back("colors.curTrack = darkgray");
-            configLines.push_back("colors.default = AAAAAA");
-            configLines.push_back("colors.exceed = darkred");
-            configLines.push_back("colors.panBG = darkgray");
-            configLines.push_back("invertImages = 1");
-        }
-    };
-
-    class LightSkin : public Skin
-    {
-    public:
-        LightSkin(){init();}
-        virtual void init()
-        {
-            configLines.push_back("colors.background = white");
-            configLines.push_back("colors.curBar = black");
-            configLines.push_back("colors.curBeat = darkgray");
-            configLines.push_back("colors.curString = gray");
-            configLines.push_back("colors.curTrack = gray");
-            configLines.push_back("colors.default = 888888");
-            configLines.push_back("colors.exceed = darkred");
-            configLines.push_back("colors.panBG = dark");
-            configLines.push_back("invertImages = 0");
-        }
-    };
-
-    class ClassicSkin : public Skin
-    {
-    public:
-        ClassicSkin(){init();}
-        virtual void init()
-        {
-            configLines.push_back("colors.background = green");
-            configLines.push_back("colors.curBar = darkblue");
-            configLines.push_back("colors.curBeat = yellow");
-            configLines.push_back("colors.curString = yellow");
-            configLines.push_back("colors.curTrack = blue");
-            configLines.push_back("colors.default = black");
-            configLines.push_back("colors.exceed = darkred");
-            configLines.push_back("colors.panBG = gray");
-            configLines.push_back("invertImages = 0");
-        }
-    };
-
-    class ClassicInvertedSkin : public Skin
-    {
-    public:
-        ClassicInvertedSkin(){init();}
-        virtual void init()
-        {
-            configLines.push_back("colors.background = 449966");
-            configLines.push_back("colors.curBar = darkblue");
-            configLines.push_back("colors.curBeat = yellow");
-            configLines.push_back("colors.curString = 7799FF");
-            configLines.push_back("colors.curTrack = blue");
-            configLines.push_back("colors.default = black");
-            configLines.push_back("colors.exceed = darkred");
-            configLines.push_back("colors.panBG = gray");
-            configLines.push_back("invertImages = 1");
-        }
-    };
-
 
 }
 
